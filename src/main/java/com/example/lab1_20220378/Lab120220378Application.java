611a5a2c0917298a6021c6e90e975e39e6a5d635
Xolmo <a20220378@pucp.edu.pe>
2025-04-04 20:47:45 -0500

    main

---
+++ src/main/java/com/example/lab1_20220378/Lab120220378Application.java
@@ -0,0 +1,13 @@
+package com.example.lab1_20220378;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab120220378Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab120220378Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lab1_20220378/Lab120220378ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab1_20220378;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab120220378ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
